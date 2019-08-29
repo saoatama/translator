b7074fa47f246955db11d95132a24457b2749d48
saoatama <riotgarnet21@gmail.com>
2019-08-29 12:39:32 +0000

    add deviseのカスタマイズ

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   namespace :private do
     resources :articles
   end
-  devise_for :users
+  
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
